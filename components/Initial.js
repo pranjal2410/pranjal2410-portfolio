99ed96e1c3989166e3286b6089cda39054f12856
pranjal2410 <newalkarpranjal2410.pn@gmail.com>
2021-04-30 10:32:25 +0530

    Added framer motion

--- components/Initial.js
+++ components/Initial.js
@@ -1,20 +1,75 @@
-import {Grid, Typography} from "@material-ui/core";
-import {motion} from "framer-motion";
-import simpleIcons from "simple-icons";
+import {Grid, Typography, makeStyles, Avatar, Zoom, Tooltip, useTheme} from "@material-ui/core";
+import {motion, AnimatePresence} from "framer-motion";
+import React, {useEffect, useState} from "react";
 
-const Initial = () => {
+const useStyles = makeStyles(theme => ({
+    container: {
+        padding: '1%',
+        textAlign: 'center',
+    },
+    title: {
+        margin: '0',
+        lineHeight: '1.15',
+        fontSize: '4rem',
+    }
+}));
+
+const Initial = ({ iconData, titles }) => {
+    const classes = useStyles();
+    const theme = useTheme();
+    const [index, setIndex] = useState(0);
+
+    useEffect(() => {
+        let interval = setInterval(() => setIndex((index+1)%titles.length), 4000);
+        return () => clearInterval(interval);
+    })
+
     return (
-        <Grid item container direction="column" alignItems="center">
+        <Grid item container direction="column" justify="center" alignItems="center" className={classes.container} spacing={2}>
             <Grid item xs={12}>
                 <motion.div
                     initial={{ y: -250 }}
                     animate={{ y: 0 }}
-                    transition={{ delay: 0.2, duration: 2, type: 'spring', stiffness: 250 }}
+                    transition={{ delay: 0.2, duration: 0.5, type: 'spring', stiffness: 250 }}
                 >
-                    <Typography component='h1' variant='h2' color='textPrimary'>
-                        Pranjal Newalkar
-                    </Typography>
+                    <h1 className={classes.title}>
+                        Hello! This is Pranjal Newalkar
+                    </h1>
                 </motion.div>
+            </Grid>
+            <Grid item xs={12}>
+                <AnimatePresence exitBeforeEnter={true}>
+                    {titles.map((title, i) => index===i && (
+                        <motion.div
+                            key={i}
+                            initial={{ opacity: 0 }}
+                            animate={{ opacity: 1 }}
+                            exit={{ opacity: 0}}
+                        >
+                            <Typography variant='h5'>{title}</Typography>
+                        </motion.div>
+                    ))}
+                </AnimatePresence>
+            </Grid>
+            <Grid item container direction="row" justify="center" spacing={2} xs={12}>
+                {iconData.map(({alt, url, path, hex, title }, i) => {
+                    return (
+                        <Grid item key={i}>
+                            <a href={url} target='_blank' rel='noopener noreferrer'>
+                                <motion.div initial={{ opacity: 0}} animate={{ opacity: 1, rotate: 360}} transition={{ delay: 1*i}}>
+                                    <Tooltip title={alt}>
+                                        <Avatar variant='rounded' style={{ backgroundColor: `#${hex}`, height: theme.spacing(8), width: theme.spacing(8), padding: theme.spacing(2)}}>
+                                            <svg role="img" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                                                <title>{title}</title>
+                                                <path d={path} fill="white"/>
+                                            </svg>
+                                        </Avatar>
+                                    </Tooltip>
+                                </motion.div>
+                            </a>
+                        </Grid>
+                    )
+                })}
             </Grid>
         </Grid>
     )
--- pages/index.js
+++ pages/index.js
@@ -3,33 +3,60 @@ import {motion} from "framer-motion";
 import {Brightness4, Brightness7} from '@material-ui/icons';
 import {useContext} from "react";
 import {ThemeContext} from "../components/theme";
+import simpleIcons from "simple-icons";
+import {data, titles} from "../components/initial.json";
 import Initial from "../components/Initial";
 
-const MainApp = () => {
+export async function getStaticProps() {
+    let iconData = [];
+    data.map(({ alt, slug, url}, i) => {
+        let icon = simpleIcons.get(slug);
+        iconData.push({
+            alt: alt,
+            url: url,
+            path: icon.path,
+            hex: icon.hex,
+            title: icon.title
+        });
+    });
+
+    return {
+        props: {
+            iconData: iconData,
+            titles: titles,
+        }
+    }
+}
+
+const MainApp = ({ iconData , titles}) => {
     const {theme, toggleTheme} = useContext(ThemeContext);
     return (
         <div style={{ flexGrow: 1, padding: '1%'}}>
-            <AppBar style={{ boxShadow: 'none'}} color='transparent' position='fixed'>
+            <AppBar style={{ boxShadow: 'none'}} color='inherit' position='fixed'>
                 <Toolbar>
-                    <IconButton color='inherit' onClick={toggleTheme}>
+                    <Typography variant='h6' style={{ flexGrow: 1}}>
+                        Portfolio
+                    </Typography>
+                    <IconButton color='inherit' onClick={toggleTheme} edge="end">
                         {theme.palette.type==='dark'?(<Brightness7/>):(<Brightness4/>)}
                     </IconButton>
                 </Toolbar>
             </AppBar>
+            <Toolbar/>
             <Grid container direction="column" alignItems="center">
-                <Initial/>
+                <Initial iconData={iconData} titles={titles}/>
             </Grid>
         </div>
     )
 }
 
-export default function Home() {
+export default function Home({ iconData }) {
     const {theme} = useContext(ThemeContext)
 
     return (
         <MuiThemeProvider theme={theme}>
             <CssBaseline/>
-            <MainApp/>
+            <MainApp iconData={iconData} titles={titles}/>
         </MuiThemeProvider>
     )
 }
