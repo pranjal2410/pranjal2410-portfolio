ef0457e431cb1b91b948ebdb34907fb2a6c1a6f7
pranjal2410 <newalkarpranjal2410.pn@gmail.com>
2021-04-30 07:21:45 +0530

    modifications done

---
+++ components/theme.js
@@ -0,0 +1,38 @@
+import {createContext, useState} from 'react';
+import {createMuiTheme} from "@material-ui/core";
+
+const ThemeContext = createContext();
+
+const ThemeContextProvider = (props) => {
+    const dark = createMuiTheme({
+        palette: {
+            type: 'dark',
+            primary: {
+                main: '#00c0ff'
+            }
+        }
+    });
+
+    const light = createMuiTheme({
+        palette: {
+            type: 'light',
+            primary: {
+                main: '#0070f3'
+            }
+        }
+    });
+
+    const [theme, setTheme] = useState(light);
+
+    const toggleTheme = () => {
+        setTheme(theme.palette.type === 'light'? dark : light);
+    }
+
+    return (
+        <ThemeContext.Provider value={{ theme, toggleTheme}}>
+            {props.children}
+        </ThemeContext.Provider>
+    )
+}
+
+export { ThemeContext, ThemeContextProvider};
--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,28 @@
-import '../styles/globals.css'
+import {ThemeContext, ThemeContextProvider} from "../components/theme";
+import {MuiThemeProvider, CssBaseline} from "@material-ui/core";
+import React, {useContext, useEffect} from "react";
+import Head from "next/head";
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+function MyApp({Component, pageProps}){
+
+    useEffect(() => {
+        // Remove the server-side injected CSS.
+        const jssStyles = document.querySelector('#jss-server-side');
+        if (jssStyles) {
+            jssStyles.parentElement.removeChild(jssStyles);
+        }
+    }, []);
+
+    return (
+        <React.Fragment>
+            <Head>
+                <title>Pranjal Newalkar</title>
+            </Head>
+            <ThemeContextProvider>
+                <Component {...pageProps} />
+            </ThemeContextProvider>
+        </React.Fragment>
+    )
 }
 
-export default MyApp
+export default MyApp;
